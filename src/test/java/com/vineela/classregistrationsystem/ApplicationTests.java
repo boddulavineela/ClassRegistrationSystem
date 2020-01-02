323e948b65e73fe673fd93a0d3193536510a3788
Boddula <BODDULVX@legal.regn.net>
2020-01-02 10:41:18 -0500

    Add test to auto import roles

--- src/test/java/com/vineela/classregistrationsystem/ApplicationTests.java
+++ src/test/java/com/vineela/classregistrationsystem/ApplicationTests.java
@@ -2,15 +2,37 @@ package com.vineela.classregistrationsystem;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlGroup;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import java.util.List;
+import java.util.Map;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@WebAppConfiguration
+@SqlGroup({
+		@Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, scripts = "classpath:roles.sql")})
 public class ApplicationTests {
+
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
 
 	@Test
 	public void contextLoads() {
+
+		String selectQuery = "SELECT * from roles WHERE id = 1";
+
+		List<Map<String, Object>> resultSet = jdbcTemplate
+				.queryForList(selectQuery);
+
+		System.out.println(resultSet);
 	}
+
 
 }
